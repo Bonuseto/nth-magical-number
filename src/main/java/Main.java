5085d5d27539478309f447b917f99a7bcfdd8c6e
Yevgeniy Fomin <jfomin@redhat.com>
2019-04-18 16:36:39 +0200

    BitSet solution in progress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,77 +1,21 @@
+import java.util.BitSet;
+
 public class Main {
 
-    public static int[] mergeRemoveDupSortIt(int[] a, int[] b) {
-        int[] c = mergeIt(a, b);
-        int[] d = removeIt(c);
-        int[] e = sortIt(d);
-        return e;
-    }
-
-    private static int[] mergeIt(int[] a, int[] b) {
-        int[] c = new int[a.length + b.length];
-        int k = 0;
-        for (int n : a) {
-            c[k++] = n;
-        }
-        for (int n : b) {
-            c[k++] = n;
-        }
-        return c;
-    }
-
-    private static int[] removeIt(int[] c) {
-        int len = c.length;
-        for (int i = 0; i < len - 1; i++) {
-            for (int j = i + 1; j < len; j++) {
-                if (c[i] == c[j]) {
-                    for (int k = j; k < len - 1; k++) {
-                        c[k] = c[k + 1];
-                    }
-                    --len;
-                }
-            }
-        }
-        int[] r = new int[len];
-        for (int i = 0; i < r.length; i++) {
-            r[i] = c[i];
-        }
-        return r;
-    }
-
-    private static int[] sortIt(int[] a) {
-        for (int index = 0; index < a.length - 1; index++) {
-            for (int i = index + 1; i < a.length; i++) {
-                if (a[index] > a[i]) {
-                    int temp = a[index];
-                    a[index] = a[i];
-                    a[i] = temp;
-                }
-            }
-        }
-        return a;
-    }
-
-    public static int nthMagicalNumber(int N, int A, int B) {
-
+    public static int bitSet (int N, int A, int B){
         int a = A;
         int b = B;
-
-        int[] divisibleByA = new int[N]; //numbers divisible by A
-        int[] divisibleByB = new int[N]; //numbers divisible by A
-        for (int i = 0; i < divisibleByA.length; i++) {
-            divisibleByA[i] = A;
+        BitSet array = new BitSet(1000000000);
+        for (int i = 0; i < N ; i++) {
+            array.set(A, true);
+            array.set(B, true);
             A = A + a;
-        }
-        for (int i = 0; i < divisibleByB.length; i++) {
-            divisibleByB[i] = B;
             B = B + b;
         }
-
-        int[] arr = mergeRemoveDupSortIt(divisibleByA, divisibleByB); //merge to arrays and remove duplicates
-        return arr[N - 1];
+        return N;
     }
 
     public static void main(String[] args) {
-        int result = nthMagicalNumber(5, 2, 4);
+        bitSet(5,9,2);
     }
 }
